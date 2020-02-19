69de46ece724b9c7f7b785bed1f4363494777894
ikunaga <ikmikm802@gmail.com>
2020-02-19 13:06:36 +0900

    [change]index.php2

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+//リクエスト先URL
+const API_URL = "https://api.line.me/v2/oauth/accessToken";
 
-$inputString = file_get_contents('php://input');
-error_log($inputString);
+$data = array(
+    'grant_type' => 'client_credentials',
+    'client_id' => 1653864146,
+    'client_secret' => 94062b4fc779ff2e28d965c45662ac35,
+);
 
+$header = array(
+    "Content-Type: application/x-www-form-urlencoded",
+);
+
+$options = array('http' => array(
+    'method' => 'POST',
+    'header'  => implode("\r\n", $header),
+    'content' => http_build_query($data)
+));
+
+$response = file_get_contents(
+    API_URL,
+    false,
+    stream_context_create($options)
+);
+
+//レスポンスのjsonからtokenを取得
+$access_token = json_decode($response)->access_token;
 ?>
