322c08d381b8720a95fec4f8be611e46fa55073f
ikunaga <ikmikm802@gmail.com>
2020-03-06 14:00:27 +0900

    20200306

--- index.php
+++ index.php
@@ -1,73 +1,51 @@
 <?php
-
-//[1]署名の検証
-   //composerでインストールしたライブラリを読み込み
-   require_once __DIR__ . '/vendor/autoload.php';
-
-   //アクセストークンを使いCurlHTTPClientをインスタンス化
-   $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
-
-   //CurlHTTPClientとシークレットを使いLINEbotをインスタンス化
-   $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
-
-   //LINEMessagingAPIがリクエストに付与した署名を取得
-   $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-
-   //署名が正当かチェック。正当であればリクエストをパースし配列へ
-   try {
-      $events = $bot->parseEventRequest(file_get_contents('php://input'),$signature);
-
-   } catch(\LINE\LINEBot\Exception\InvalidSignatureException $e) {
-      error_log('parseEventRequest failed. InvalidSignatureException => '.var_export($e, true));
-   } catch(\LINE\LINEBot\Exception\UnknownEventTypeException $e) {
-      error_log('parseEventRequest failed. UnknownEventTypeException => '.var_export($e, true));
-   } catch(\LINE\LINEBot\Exception\UnknownMessageTypeException $e) {
-      error_log('parseEventRequest failed. UnknownMessageTypeException => '.var_export($e, true));
-   } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
-      error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
-   }
-
-//[2]メッセージタイプのフィルタ
-
-// 配列に格納された各イベントをループで処理
-foreach ($events as $event) {
-   // テキストを返信
-   $bot->replyText($event->getReplyToken(), $event->getText());
-
-
-   //テキストを返信し次のイベントの処理へ
-   // replyTextMessage($bot, $event->getReplyToken(), 'TextMessage');
-
-   //画像を返信
-   // replyTextMessage($bot, $event->getReplyToken(), 'https://' . 
-   // $_SERVER['HTTP_HOST'] . 
-   // '/imgs/tsol_logo.jpg',
-   // 'https://' . $_SERVER['HTTP_HOST'] . 
-   // '/imgs/tower.jpg');
-}
-
-
-
-//テキストを返信。引数はLINEbot、返信先、テキスト
-function replyTextMessage($bot, $replyToken, $text) {
-   //返信を行いレスポンスを取得
-   //TextMessageBuilderの引数はテキスト
-   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($text));
-
-   //レスポンスが異常な場合
-   if (!$response->isSucceeded()) {
-      //エラー内容を出力
-      error_log('Failed!'. $response->getHTTPStatus . '　' . $response->getRawBody());
-
-   }
-}
-
-//画像を返信。引数はLINEBot、返信先、画像URL、サムネイルURL
-function replyImageMessage($bot, $replyToken, $originalImageUrl, $previewImageUrl) {
-   //ImageMessageBuilderの引数は画像URL、サムネイルURL
-   $response = $bot->replyMessage($replyToken, new \LINE\LINEBot\MessageBuilder\ImageMessageBuilder($originalImageUrl, $previewImageUrl));
-   if (!$response->isSucceeded()) {
-      error_log('Failed!'. $response->getHTTPStatus . '　' . $response->getRawBody());
-   }
+ 
+$accessToken = 'Ifx/DI3phSRtgW8Isa5AZVfzBizBDu4TEPRtyORDEqp3FaCcIWYXg4CubsYgfN0Iqm8pwOP4NMvNg+DeIDz0N8VxT3HArA2+kWoLDDGv1MwAcPaTVnhgKSVrdwrwAArb8LsWswOoHADrs/4i0fIdewdB04t89/1O/w1cDnyilFU=';
+ 
+//ユーザーからのメッセージ取得
+$json_string = file_get_contents('php://input');
+$json_object = json_decode($json_string);
+ 
+//取得データ
+$replyToken = $json_object->{"events"}[0]->{"replyToken"};        //返信用トークン
+$message_type = $json_object->{"events"}[0]->{"message"}->{"type"};    //メッセージタイプ
+$message_text = $json_object->{"events"}[0]->{"message"}->{"text"};    //メッセージ内容
+ 
+//メッセージタイプが「text」以外のときは何も返さず終了
+if($message_type != "text") exit;
+ 
+//返信メッセージ
+$return_message_text = "「" . $message_text . "」じゃねーよｗｗｗ";
+ 
+//返信実行
+sending_messages($accessToken, $replyToken, $message_type, $return_message_text);
+?>
+<?php
+//メッセージの送信
+function sending_messages($accessToken, $replyToken, $message_type, $return_message_text){
+    //レスポンスフォーマット
+    $response_format_text = [
+        "type" => $message_type,
+        "text" => $return_message_text
+    ];
+ 
+    //ポストデータ
+    $post_data = [
+        "replyToken" => $replyToken,
+        "messages" => [$response_format_text]
+    ];
+ 
+    //curl実行
+    $ch = curl_init("https://api.line.me/v2/bot/message/reply");
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json; charser=UTF-8',
+        'Authorization: Bearer ' . $accessToken
+    ));
+    $result = curl_exec($ch);
+    curl_close($ch);
 }
 ?>
